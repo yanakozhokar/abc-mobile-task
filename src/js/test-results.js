5b923a51da13e7a29444d56ca215d138ab7060fa
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-04-24 22:35:34 +0300

    add fetch

--- src/js/test-results.js
+++ src/js/test-results.js
@@ -1,4 +1,97 @@
+const callBtnRef = document.querySelector('.results__call-btn');
 const resultsTimerRef = document.querySelector('.results__timer');
+
+callBtnRef.addEventListener('click', onCallBtnClick);
+
+function onCallBtnClick() {
+  fetch(`https://swapi.dev/api/people/1/`)
+    .then(data => data.json())
+    .then(data => {
+      addMarkup(data);
+    });
+}
+
+function addMarkup(data) {
+  const {
+    name,
+    height,
+    mass,
+    hair_color,
+    skin_color,
+    eye_color,
+    birth_year,
+    gender,
+    homeworld,
+    films,
+    species,
+    vehicles,
+    starships,
+    created,
+    edited,
+    url,
+  } = data;
+
+  const markup = `<div class="results__request">
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Name: </span>${name}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Height: </span>${height}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Mass: </span>${mass}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Hair color: </span>${hair_color}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Skin color: </span>${skin_color}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Eye color: </span>${eye_color}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Birth year: </span>${birth_year}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Gender: </span>${gender}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Homeworld: </span>${homeworld}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Films: </span>${films
+          .map(el => el)
+          .join(' ')}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Species: </span>${species
+          .map(el => el)
+          .join(' ')}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Vehicles: </span>${vehicles
+          .map(el => el)
+          .join(' ')}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Starships: </span>${starships
+          .map(el => el)
+          .join(' ')}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Created: </span>${created}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Edited: </span>${edited}
+      </p>
+      <p class="results__request-characteristic">
+        <span class="bold-lowercase">Url: </span>${url}
+      </p>
+    </div>`;
+
+  callBtnRef.insertAdjacentHTML('afterend', markup);
+}
 
 class Timer {
   constructor() {
